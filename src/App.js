144db51bf6bd3cad16522448b8e9c3dda15d9272
numberwang55 <cban0901@hotmail.com>
2023-08-08 17:54:11 +0100

    bulma.io

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
 
 export default function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title={"Alexa"}
+                handle={"@alexa99"}
+                image={AlexaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title={"Cortana"}
+                handle={"@cortana32"}
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title={"Siri"}
+                handle={"@siri01"}
+                image={SiriImage}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
